0f3120bf9f7fc91f33153d0be5929b53d7b67e76
Pasha Medvedev <pa.medvedev@tinkoff.ru>
2025-05-14 17:30:37 +0300

    feat: добавление функционала выбора места для обеда и настройка стиля диалога
    - Добавлен выбор между кухней и другим местом для обеда с соответствующими кнопками и полем для ввода кастомного места.
    - Настроены стили и поведение BottomSheetDialog для корректного отображения и взаимодействия.
    - Обновлены XML-разметки для улучшения интерфейса и добавления необходимых атрибутов.

--- app/src/main/java/ru/tinkoff/lunch/screens/main/ui/dialogs/CreateLunchBottomSheet.kt
+++ app/src/main/java/ru/tinkoff/lunch/screens/main/ui/dialogs/CreateLunchBottomSheet.kt
@@ -1,11 +1,16 @@
 package ru.tinkoff.lunch.screens.main.ui.dialogs
 
+import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import ru.tinkoff.lunch.R
 import ru.tinkoff.lunch.databinding.FragmentBottomSheetCreateLunchBinding
 import ru.tinkoff.lunch.screens.main.ui.dialogs.recycler.CreateLunchBottomSheetViewHolder
 import ru.tinkoff.lunch.screens.main.ui.dialogs.recycler.model.ItemLunchTime
@@ -19,9 +24,24 @@ class CreateLunchBottomSheet(
     private val binding by lazy { FragmentBottomSheetCreateLunchBinding.inflate(layoutInflater) }
     private var listener: CreateLunchBottomSheetListener? = listener
     private lateinit var recycler: TiRecyclerCoroutines<ViewTyped>
+    private var isKitchenSelected = true
 
     init {
         this.listener = listener
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.DialogStyle)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val bottomSheetDialog = super.onCreateDialog(savedInstanceState)
+        if (bottomSheetDialog is BottomSheetDialog) {
+            bottomSheetDialog.behavior.skipCollapsed = true
+            bottomSheetDialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+        }
+        return bottomSheetDialog
     }
 
     override fun onCreateView(
@@ -34,6 +54,32 @@ class CreateLunchBottomSheet(
         super.onViewCreated(view, savedInstanceState)
         bindActions()
         initRecycler()
+
+        binding.otherPlaceButton.setOnClickListener {
+            if (isKitchenSelected) {
+                binding.kitchenButton.apply {
+                    isKitchenSelected = false
+                    setBackgroundColor(requireContext().getColor(R.color.chip_gray))
+                }
+                binding.otherPlaceButton.apply {
+                    setBackgroundColor(requireContext().getColor(R.color.yellow))
+                }
+                binding.customPlaceField.isVisible = true
+            }
+        }
+
+        binding.kitchenButton.setOnClickListener {
+            if (!isKitchenSelected) {
+                binding.kitchenButton.apply {
+                    isKitchenSelected = true
+                    setBackgroundColor(requireContext().getColor(R.color.yellow))
+                }
+                binding.otherPlaceButton.apply {
+                    setBackgroundColor(requireContext().getColor(R.color.chip_gray))
+                }
+                binding.customPlaceField.isVisible = false
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
